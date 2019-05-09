afc23e38cc4dc26e67456f106cb0dce79f45aae2
zhanglxlx <zhanglx@tedu.cn>
2019-05-09 19:52:45 +0800

    foot二

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(() => {
+    // 引入页面头部
     $('<link rel="stylesheet" href="css/base.css">').appendTo("head");
     $('<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">').appendTo("head");
     $.ajax({
@@ -63,5 +64,13 @@ $(() => {
         `
     }
     $("#setions").append(html1)
-   
+    // 引入页面尾部
+    $.ajax({
+        url: 'footer.html',
+        type: 'get',
+        success: function(res) {
+            // console.log(res);
+            $("#footer").append(res);
+        }
+    })
 })
